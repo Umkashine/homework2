a6839dbe7276b2f91eb46cf3cd3468d28816100c
Umkashine <dimavmide@gmail.com>
2023-08-28 23:58:44 +0300

    задача 3

---
+++ task3.java
@@ -0,0 +1,10 @@
+public class task3 {
+    public static void main(String[] args) {
+        int n = 126;
+        int q = n / 100;
+        int w = n / 10;
+        int e = w % 10;
+        int r = n % 10;
+System.out.println(q + e + r);
+ }
+}
