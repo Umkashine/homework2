bd24ee3aa2f792a44e465eeab39b9736cfe908cf
Umkashine <dimavmide@gmail.com>
2023-08-28 23:46:04 +0300

    g76yf76f

---
+++ task2.java
@@ -0,0 +1,8 @@
+public class task2 {
+    public static void main(String[] args) {
+        int n = 26;
+        int x = n / 10;
+        int o = n % 10;
+        System.out.println(x + o);
+    }
+}ft7fuytfuy
