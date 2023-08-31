ead953130ac348376745dbd9ee1018a8b98f5ff6
Umkashine <dimavmide@gmail.com>
2023-08-31 21:30:55 +0300

    v probel

---
+++ while25.java
@@ -0,0 +1,11 @@
+public class while25 {
+    public static void main (String[] args) {
+        int a = 0;
+        int b = 30;
+
+        while (a < 26 ){
+            a++;
+            System.out.print(a +" ");
+        }
+    }
+}
