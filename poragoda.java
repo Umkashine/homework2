bebfb0197221fbfdc36da499b278022a1e92c686
Umkashine <dimavmide@gmail.com>
2023-08-31 20:55:29 +0300

    ot 1 do 12 mesaci

---
+++ poragoda.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class poragoda {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int a = scanner.nextInt();
+        switch (a){
+            case 1: {
+                System.out.println("yanvar");
+                break;
+            }
+            case 2: {
+                System.out.println("fevral");
+                break;
+            }
+            case 3: {
+                System.out.println("mart");
+                break;
+            }
+            case 4: {
+                System.out.println("april");
+                break;
+            }
+            case 5: {
+                System.out.println("may");
+                break;
+            }
+            case 6: {
+                System.out.println("iyn");
+                break;
+            }
+            case 7: {
+                System.out.println("yul");
+                break;
+            }
+            case 8: {
+                System.out.println("august");
+                break;
+            }
+            case 9: {
+                System.out.println("september");
+                break;
+            }
+            case 10: {
+                System.out.println("october");
+                break;
+            }
+            case 11: {
+                System.out.println("november");
+                break;
+            }
+            case 12: {
+                System.out.println("december");
+                break;
+            }
+        }
+    }
+}
